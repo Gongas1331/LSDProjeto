803520dfa3a1889c8b357da19f64f3bc8c8a4fde
Gonçalo Cunha <goncalo.cunha@ua.pt>
2022-06-13 16:54:59 +0100

    Update register.vhd

--- register.vhd
+++ register.vhd
@@ -6,12 +6,12 @@ entity register is
     port(
         clk : in std_logic;
         startstop : in std_logic;
-        p1 : in std_logic_vector;
-        p2 : in std_logic_vector;
-        p3 : in std_logic_vector;
-        startstop : out std_logic;
-        p1 : out std_logic_vector;
-        p2 : out std_logic_vector;
-        p3 : out std_logic_vector;
+        ip1 : in std_logic_vector;
+        ip2 : in std_logic_vector;
+        ip3 : in std_logic_vector;
+        ostartstop : out std_logic;
+        op1 : out std_logic_vector;
+        op2 : out std_logic_vector;
+        op3 : out std_logic_vector;
         )
 end register;
